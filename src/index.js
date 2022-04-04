48e2bb9638233a199c5528e8ebc3110b61a089b3
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-04-04 17:13:15 -0400

    formatting of nav bar

--- src/index.js
+++ src/index.js
@@ -7,23 +7,31 @@ import createMenuPage from './pages/menu.js';
 function navbar(){
     const element = document.createElement('div');
     element.setAttribute('id','navigation')
+    const navElement = document.createElement('nav')
+    navElement.setAttribute('class','navbar navbar-expand-lg')
     const listStart = document.createElement('ul');
-    listStart.className = 'tab-list'
+    listStart.classList.add("nav","tab-list")
     const listElement1 = document.createElement('li');
     listElement1.setAttribute('id','home');
+    listElement1.setAttribute('class','nav-item');
     listElement1.innerHTML = 'Home'
     const listElement2 = document.createElement('li');
     listElement2.setAttribute('id','menu');
+    listElement2.setAttribute('class','nav-item');
     listElement2.innerHTML = 'Menu'
     const listElement3 = document.createElement('li');
     listElement3.setAttribute('id','contactUs');
+    listElement3.setAttribute('class','nav-item');
     listElement3.innerHTML = 'Contact Us'
 
     listStart.appendChild(listElement1);
     listStart.appendChild(listElement2);
     listStart.appendChild(listElement3);
 
-    element.appendChild(listStart);
+    navElement.appendChild(listStart)
+
+    element.appendChild(navElement);
+
 
     return element;
 
