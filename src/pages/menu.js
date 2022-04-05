5a1f913632c211b9fc5ca61a1a2fa0f0bc2fe3a8
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-04-05 00:31:03 -0400

    add all menu items

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -19,6 +19,27 @@ const blackTeaDescription = 'A true classic and founder favorite';
 const blackTeaPrice = '$4.95';
 element.appendChild(addMenuItem(blackTeaImageURL,blackTeaName, blackTeaDescription, blackTeaPrice));
 
+//adding item2
+const peachTeaImageURL = '../src/images/peachtea.png';
+const peachTeaName = 'Peach Tea';
+const peachTeaDescription = "Surprise your SO with this drink to show them your ap-peach-iation <3";
+const peachTeaPrice = '$4.95';
+element.appendChild(addMenuItem(peachTeaImageURL,peachTeaName, peachTeaDescription, peachTeaPrice));
+
+//adding item3
+const mangoTeaImageURL = '../src/images/mangoTea.png';
+const mangoTeaName = 'Mango Tea';
+const mangoTeaDescription = "Sunshine in a cup, need I say more?";
+const mangoTeaPrice = '$4.95';
+element.appendChild(addMenuItem(mangoTeaImageURL,mangoTeaName, mangoTeaDescription, mangoTeaPrice));
+
+//adding item4
+const strawberryTeaImageURL = '../src/images/strawberrytea.png';
+const strawberryTeaName = 'Strawberry Tea';
+const strawberryTeaDescription = "Level up the strawberry milk of your childhood";
+const strawberryTeaPrice = '$4.95';
+element.appendChild(addMenuItem(strawberryTeaImageURL,strawberryTeaName, strawberryTeaDescription, strawberryTeaPrice));
+
 return element;
 }
 
